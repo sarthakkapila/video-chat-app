be4df069d0698e880a241198d08b5da5fd4120a3
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-02-14 22:52:49 +0530

    -

--- Server/src/managers/RoomManager.ts
+++ Server/src/managers/RoomManager.ts
@@ -18,5 +18,49 @@ export class RoomManager {
             user1,
             user2
         })
+        user1.socket.emit("send-offer", {
+            roomid
+        })
+
+        user2.socket.emit("send-offer", {
+            roomid
+        })
     }
+
+    onOffer(SenderSocketid: string, sdp: string, roomid: string){
+        const room = this.rooms.get(roomid);
+        if(!roomid){
+            return
+        }
+        const ReceivingUser = (room?.user1.socket.id == SenderSocketid) ? room.user2 : room?.user1
+
+        ReceivingUser?.socket.emit("offer", {
+            sdp,
+            roomid
+        })
+
+    }
+
+    onAnswer(SenderSocketid: string, sdp: string, roomid: string){
+        const room = this.rooms.get(roomid);
+        if(!roomid){
+            return
+        }
+        const ReceivingUser = (room?.user1.socket.id == SenderSocketid) ? room.user2 : room?.user1
+
+        ReceivingUser?.socket.emit("answer", {
+            sdp,
+            roomid
+        })
+    }
+
+    onIceCandidates(roomId: string, senderSocketid: string, candidate: any, type: "sender" | "receiver") {
+        const room = this.rooms.get(roomId);
+        if (!room) {
+            return;
+        }
+        const receivingUser = room.user1.socket.id === senderSocketid ? room.user2: room.user1;
+        receivingUser.socket.emit("add-ice-candidate", ({candidate, type}));
+    }
+
 }
--- Server/src/managers/UserManager.ts
+++ Server/src/managers/UserManager.ts
@@ -1,13 +1,18 @@
 import { Socket } from "socket.io"
-
+import { RoomManager } from "./RoomManager";
 export interface User {
     name: string
     socket: Socket
 }
 
 export class UserManager {
-    private users: Map<string, User>
-    constructor() {
-        this.users = new Map<string, User>()
+    private users: User[];
+    private queue: string[];
+    private roomManager: RoomManager;
+
+        constructor() {
+            this.users = [];
+            this.queue = [];
+            this.roomManager = new RoomManager();
     }
 }
